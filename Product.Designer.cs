504920c52a9914a02e6324716df1056bc1e45324
Reean <44005421+reeantencamah@users.noreply.github.com>
2020-06-02 19:11:18 +0400

    Add files via upload

---
+++ Product.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WholesalerSupplierR
+{
+    partial class Product
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtTotalCost = new System.Windows.Forms.TextBox();
+            this.txtUnitCost = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvProduct = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduct)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrder.Location = new System.Drawing.Point(751, 151);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(109, 33);
+            this.btnOrder.TabIndex = 22;
+            this.btnOrder.Text = "Order";
+            this.btnOrder.UseVisualStyleBackColor = true;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkCyan;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Insert quantity:";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(145, 16);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 27);
+            this.txtQuantity.TabIndex = 20;
+            this.txtQuantity.TextChanged += new System.EventHandler(this.txtQuantity_TextChanged);
+            // 
+            // txtTotalCost
+            // 
+            this.txtTotalCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalCost.Location = new System.Drawing.Point(145, 99);
+            this.txtTotalCost.Name = "txtTotalCost";
+            this.txtTotalCost.ReadOnly = true;
+            this.txtTotalCost.Size = new System.Drawing.Size(191, 27);
+            this.txtTotalCost.TabIndex = 19;
+            // 
+            // txtUnitCost
+            // 
+            this.txtUnitCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnitCost.Location = new System.Drawing.Point(145, 66);
+            this.txtUnitCost.Name = "txtUnitCost";
+            this.txtUnitCost.ReadOnly = true;
+            this.txtUnitCost.Size = new System.Drawing.Size(191, 27);
+            this.txtUnitCost.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DarkCyan;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(32, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Total Cost:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkCyan;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Unit Cost:";
+            // 
+            // dgvProduct
+            // 
+            this.dgvProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProduct.Location = new System.Drawing.Point(12, 220);
+            this.dgvProduct.Name = "dgvProduct";
+            this.dgvProduct.RowTemplate.Height = 24;
+            this.dgvProduct.Size = new System.Drawing.Size(877, 386);
+            this.dgvProduct.TabIndex = 15;
+            this.dgvProduct.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProduct_CellContentClick);
+            this.dgvProduct.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProduct_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Search:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(155, 8);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 27);
+            this.txtSearch.TabIndex = 13;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(291, 8);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 27);
+            this.btnSearch.TabIndex = 24;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.txtUnitCost);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtQuantity);
+            this.panel1.Controls.Add(this.txtTotalCost);
+            this.panel1.Location = new System.Drawing.Point(439, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 202);
+            this.panel1.TabIndex = 25;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(295, 15);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(72, 28);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Product
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(902, 618);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnOrder);
+            this.Controls.Add(this.dgvProduct);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.panel1);
+            this.Name = "Product";
+            this.Text = "Product";
+            this.Load += new System.EventHandler(this.Product_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduct)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtTotalCost;
+        private System.Windows.Forms.TextBox txtUnitCost;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvProduct;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WholesalerSupplierR
+{
+    public partial class Product : Form
+    {
+        public Product()
+        {
+            InitializeComponent();
+        }
+
+        DataTable dt = new DataTable();
+        int rowIndex = 0;
+
+        private void Product_Load(object sender, EventArgs e)
+        {
+            string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Pharma.mdf;Integrated Security=True;Connect Timeout=30";
+            string sqlStr = "Select * From Product";
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(sqlStr, connStr);
+            sda.Fill(dt);
+            dgvProduct.DataSource = dt;
+            sda.Dispose();
+
+            dgvProduct.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+           
+
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            string searchValue = txtSearch.Text;
+            
+            dgvProduct.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            try
+            {
+                
+                foreach (DataGridViewRow row in dgvProduct.Rows)
+                {
+                    dgvProduct.ClearSelection();
+
+
+                    if (row.Cells[1].Value.ToString().Equals(searchValue))
+                    {
+                        row.Selected = true;
+                        break;
+
+                    }
+
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+
+
+
+
+
+            
+
+            
+
+
+        }
+
+        private void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+            int num, unit;
+            if (txtQuantity.Text == "")
+                num = 0; 
+            else
+                num = int.Parse(txtQuantity.Text);
+
+            if (txtUnitCost.Text == "")
+                unit = 0;
+            else
+                unit = int.Parse(txtUnitCost.Text);
+
+
+            
+            
+
+            if (num>=0)
+            {
+                int total = num * unit;
+                txtTotalCost.Text = total.ToString();
+            }
+            else if (num<0)
+            {
+               
+                MessageBox.Show("Please insert how many of the chosen product you would order", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+                
+            
+
+
+        }
+
+        private void dgvProduct_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            DataGridViewRow row = this.dgvProduct.Rows[e.RowIndex];
+            txtUnitCost.Text = row.Cells[2].Value.ToString();
+
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Order of the selected product has been sent to Supplier!");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtQuantity.Text = txtTotalCost.Text = txtUnitCost.Text = "";
+        }
+    }
+}
